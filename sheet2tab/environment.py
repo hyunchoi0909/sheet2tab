0222eb1cd1b54488ab82eaebc715ef434ff19bda
Sean Choi <choi.hyunsean@gmail.com>
2023-07-19 18:55:25 -0400

    HC: Almost finalized file conversion; figure out dtype issues with frets

--- sheet2tab/environment.py
+++ sheet2tab/environment.py
@@ -2,10 +2,15 @@ import os
 
 class pathenv():
 	ROOT = __file__
-	DATA_RAW = os.path.join(ROOT,"rawdata")
+	DATA_RAW = os.path.join(ROOT,"..\\rawdata")
 	TRAIN_RAW = os.path.join(DATA_RAW, "train")
 	TEST_RAW = os.path.join(DATA_RAW, "test")
 
+
+class setenv():
+	#String 0 is highest string
+	COLUMNS = ["PITCH","STRING0", "STRING1", "STRING2", "STRING3", "STRING4", "STRING5", "DURATION"]
+	COLUMNS_EXTRA = ["MUTE", "HAMMERON", "PULLOFF", "SLIDE"]
 class guitarenv():
 	NUMSTRINGS = 6
 	STRINGS = [64,59,55,50,45,40]
--- sheet2tab/preprocess.py
+++ sheet2tab/preprocess.py
@@ -3,27 +3,70 @@ import numpy as np
 import tqdm
 import os
 import subprocess
+import pandas as pd
 import guitarpro
 
-def fileconvert(input_file):
+def fileconvert(input_file, file_write = True):
+	print("Begin processing: " + input_file)
 	tracks = []
 	try:
 		song = guitarpro.parse(input_file)
 		for track in song.tracks:
-			dataset = []
+			# Create dataset
+			dataset = {}
+			for column in setenv.COLUMNS:
+				dataset[column] = []
+
+			# ignore if percussion or wrong form of guitar
 			if track.isPercussionTrack: continue
 			if len(track.strings) != guitarenv.NUMSTRINGS or track.fretCount != 24: continue
 			for i in range(guitarenv.NUMSTRINGS):
 				if guitarenv.STRINGS[i] != track.strings[i].value: continue
 
+			# Add notes to dataframe
+			beginning = True
 			for measure in track.measures:
 				for beat in measure.voices[0].beats:
-					datapoint = [4.0/beat.duration.value,[-1]*guitarenv.NUMSTRINGS]
-					for note in beat.notes:
-						datapoint[1][note.string-1] = note.value
-					dataset.append(datapoint)
-			tracks.append(dataset)
+					empty_beat = beat.status == guitarpro.BeatStatus.empty
+					rest_beat = beat.status == guitarpro.BeatStatus.rest
+
+					# Skip to first note
+					if beginning and (empty_beat or rest_beat):
+						continue
+					beginning = False
+					
+					# If tie, just add duration to last note
+					#print(beat.status)
+					if empty_beat or rest_beat or beat.notes[0].type == guitarpro.NoteType.tie:
+						dataset["DURATION"][-1] +=  4.0/beat.duration.value
+
+					# Add new beat to dataset
+					else:
+						pitches = []
+						for i in range(guitarenv.NUMSTRINGS):
+							dataset["STRING" + str(i)].append(np.nan)
+						for note in beat.notes:
+							dataset["STRING" + str(note.string-1)][-1] = note.value
+							pitches.append(note.realValue)
+						dataset["DURATION"].append(4.0/beat.duration.value)
+						pitches.sort()
+						dataset["PITCH"].append(pitches)
+
+			tracks.append(pd.DataFrame(dataset))
+			print(pd.DataFrame(dataset).dtypes)
+
+				
 	except Exception as inst:
-		print("test fail with " + input_file)
-		print(inst)
+		print("Test fail with " + input_file)
+		print("Error: " + inst)	
+
+	# Write tracks to individual files as csv
+	if file_write:
+		input_prefix = os.path.splitext(input_file)[0]
+		for i, track in enumerate(tracks):
+			output_suffix = "(track_{}).csv".format(i)
+			print("\tWrite to CSV: " + input_prefix + output_suffix)
+			track.to_csv(input_prefix + output_suffix, header = False, index = False)
+	print("Processed file " + input_file)
+
 	return tracks
